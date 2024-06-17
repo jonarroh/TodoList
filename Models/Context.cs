fdece47a721ee74cd1dc747da5c16157288a473c
jonarroh <jonarrodi99@gmail.com>
2024-06-17 10:39:00 -0600

    first commit

---
+++ Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoList.Models
+{
+    public class Context: DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+          
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
